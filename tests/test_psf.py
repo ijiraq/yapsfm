c2983e4135f00ef946b46bc529cc912dd6cafed3
Chris Glass <tribaal@gmail.com>
2015-04-09 22:38:11 +0300

    Added some project boilerplate
    Including but not limited to:
    - A Makefile do speed up testing and development environment setup
    - A skeleton for unit tests
    
    I took the opinionated decision to use the nose test runner (because
    it's the best in my humble opinion), but of course any other test runner
    can be used instead since the tests only use the standard
    unittest module.
    
    This commit is provided free of charge and without any warranty, and the
    code it encompasses is hereby released to the public domain (use as you see
    fit for any purpose).

---
+++ tests/test_psf.py
@@ -0,0 +1,20 @@
+import unittest
+
+from PSFModel import Zodd
+
+
+class PSFModelTest(unittest.TestCase):
+
+    def test_zodd(self):
+        """
+        Test the Zodd function as it's the only one I can reasonably
+        understand in less that a few seconds of looking at it.
+
+        No idea what it actually does, of course. Let's not let that get in the
+        way.
+        """
+        result = Zodd(1, 1, 1, 1)
+        self.assertEqual(1.6829419696157935, result)  # Woo I'm doing astronomy
+
+    # You can add further tests by simply adding methods starting with test_*
+    # and importing your test target at the top of this file.
