5a391302d043f40c43fdf518a8c4f176727ac2ae
Florian Glass <glass.florian@gmail.com>
2015-04-10 10:43:06 -0700

    Method names changed to PEP8
    Added .fits aperture creation / input handling

--- tests/test_psf.py
+++ tests/test_psf.py
@@ -1,6 +1,6 @@
 import unittest
 
-from PSFModel import Zodd
+from PSFModel import odd_zernike
 
 
 class PSFModelTest(unittest.TestCase):
@@ -13,7 +13,7 @@ class PSFModelTest(unittest.TestCase):
         No idea what it actually does, of course. Let's not let that get in the
         way.
         """
-        result = Zodd(1, 1, 1, 1)
+        result = odd_zernike(1, 1, 1, 1)
         self.assertEqual(1.6829419696157935, result)  # Woo I'm doing astronomy
 
     # You can add further tests by simply adding methods starting with test_*
