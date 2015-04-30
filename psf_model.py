cbab9b0cbb9b9d0b89579f120c0b5c6e18dfc5e7
Florian Glass <glass.florian@gmail.com>
2015-04-30 11:35:48 -0700

    Merge branch 'master' of /home/khabal/yapsfm with conflicts.

--- psf_model.py
+++ psf_model.py
@@ -5,18 +5,16 @@ Asks for user input on *wavelength* and pixel *scale* (or resolution) in ''/pixe
 It then computes an on-axis PSF model using the file distortions.par as source of optical distortions.
 """
 
-import numpy as np
-
-from modules import Pupil, PSF
+from modules import PSF
 
 
 def main():
     wavelength = float(raw_input('wavelength? (0.76 to 2.00 microns) ') or .76)
     scale = float(raw_input("final pixel scale in ''/pixel? (0.01) ") or 0.01)
-    pupil = Pupil(wavelength)
+    # pupil = Pupil(wavelength)
 
-    psf = PSF(pupil, scale)
-    psf.resize_psf(wavelength=wavelength, size=np.shape(psf.a)[0], scale=scale)
+    psf = PSF(wavelength, scale)
+    psf.resize_psf()
     psf.save()  # does not fill the header of the .fits image (yet)
 
 if __name__ == "__main__":
