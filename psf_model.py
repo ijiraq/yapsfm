0130341ccaed10db741e8a128a9af947a212848e
Florian Glass <glass.florian@gmail.com>
2015-05-05 14:24:54 -0700

    reverted bad changes

--- psf_model.py
+++ psf_model.py
@@ -5,16 +5,18 @@ Asks for user input on *wavelength* and pixel *scale* (or resolution) in ''/pixe
 It then computes an on-axis PSF model using the file distortions.par as source of optical distortions.
 """
 
-from modules import PSF
+import numpy as np
+
+from modules import Pupil, PSF
 
 
 def main():
     wavelength = float(raw_input('wavelength? (0.76 to 2.00 microns) ') or .76)
     scale = float(raw_input("final pixel scale in ''/pixel? (0.01) ") or 0.01)
-    # pupil = Pupil(wavelength)
+    pupil = Pupil(wavelength)
 
-    psf = PSF(wavelength, scale)
-    psf.resize_psf()
+    psf = PSF(pupil, scale)
+    psf.resize_psf(wavelength=wavelength, size=np.shape(psf.a)[0], scale=scale)
     psf.save()  # does not fill the header of the .fits image (yet)
 
 if __name__ == "__main__":
